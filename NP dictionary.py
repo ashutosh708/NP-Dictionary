878579d2940a0978451fb42f09570ef68ec33c06
ashutosh708 <85440783+ashutosh708@users.noreply.github.com>
2021-06-07 09:59:41 +0530

    Add files via upload

---
+++ NP dictionary.py
@@ -0,0 +1,15 @@
+b=True
+while b==True:
+    dict={"aback":"behind,backward","abacus":"frame of wires with balls used for teaching elementary counting","abandon":"to give up","abandoned":"given up",
+          "abandonment":"complete self surrender","abase":"lower,to degrade","abasement":"an act of degrading anyone","abash":"to put to shame","abask":"a warm light",
+          "abate":"to deduct,to diminish","abatement":"reduction,decrabackease","abattior":"a place where animals are cut for meat","abbess":"female head of nunnery",
+          "abbey":"body of monks or nuns","abuse":"to use shameful words","abbot":"male head of monatery","abbreviate":"make brief"}
+    a=input("enter the word").lower()
+    
+    print(dict[a])
+    print("")
+    print("")
+    
+    continue
+
+   
